b0942782e88a7e7d8d32acbd64e4b286f36db1f6
Irina Khaustova <Irina170580@yandex.ru>
2022-03-31 22:56:53 +0300

    version1

--- src/js/test.js
+++ src/js/test.js
@@ -1,7 +1,7 @@
 import demo from './app';
 
 test('проверка работы функции', () => {
-  const received = 'привет)';
-  const expected = demo('привет)');
+  const received = 'привет';
+  const expected = demo('привет');
   expect(received).toEqual(expected);
 });

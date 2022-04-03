41e0c5652b2c621599e67f94f83cd2b9b3b05353
Irina Khaustova <Irina170580@yandex.ru>
2022-04-03 15:07:49 +0300

    2

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@
 const unusedVariable = 'variable';
 
 // for demonstration purpose only
-export default function demo(value1) {
-  return value1;
+export default function demo(value) {
+  return value;
 }
 demo(unusedVariable);
